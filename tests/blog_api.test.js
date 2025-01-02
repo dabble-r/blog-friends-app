075ffc990e5d730fc707dd12e7adca59a0e20026
Nicholas <njbroussard.1@gmail.com>
2025-01-02 13:25:25 -0600

    all test contained in describe blocks

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,4 +1,4 @@
-import { test, after, beforeEach } from 'node:test'
+import { test, after, beforeEach, describe } from 'node:test'
 import assert from 'node:assert'
 import mongoose from 'mongoose'
 import supertest from 'supertest'
@@ -17,120 +17,138 @@ beforeEach(async () => {
 })
 */
 
-// simple test, expects blogs to return as json format
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+//////////////////////////////////////////////////////////////////////
+
+describe('test for all blogs resources', () => {
+  // tests for blogs at /api/blogs
+  // simple test, expects blogs to return as json format
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  // checks blogs count from locally initialized array
+  test('total blogs count', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, initialBlogs.length)
+  })
 })
 
-// checks 'id' prop of each blog is literally id
-test('id is really id', async () => {
-  const response = await api.get('/api/blogs')
+//////////////////////////////////////////////////////////////////////
 
-  const blogIDs = response.body.map(el => el.id).filter(el => el != 'id')
+describe('test for one valid/malformed blogs resource', () => {
+  // tests for blog prop 
+  // checks 'id' prop of each blog is literally id
+  test('id is really id', async () => {
+    const response = await api.get('/api/blogs')
 
-  assert.strictEqual(response.body.length, blogIDs.length)
-})
+    const blogIDs = response.body.map(el => el.id).filter(el => el != 'id')
 
+    assert.strictEqual(response.body.length, blogIDs.length)
+  })
 
-// checks blogs count from locally initialized array
-test('total blogs count', async () => {
-  const response = await api.get('/api/blogs')
+  // must have all content input
+  test('blog malformed - invalid not added', async () => {
+    const newBlog = {
+      "comment": "",
+      "likes": "",
+      "date": ""
+    }
 
-  assert.strictEqual(response.body.length, initialBlogs.length)
-})
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
 
-// checks all blogs content from mongoDB storage
-test('blogs content check', async () => {
-  const response = await api.get('/api/blogs')
+    const blogsResult = await blogsInDb()
 
-  const contents = response.body.map(el => el.comment)
-  assert(contents.includes('I like Jason because he travels to places.'))
-})
+    assert.strictEqual(blogsResult.length, initialBlogs.length)  
+  })
 
-// checks one blogs content of first array item for mongoDB storage
-test('blogs content -- first item -- check', async () => {
-  const response = await api.get('/api/blogs')
-  const firstElContents = response.body.map(el => el.comment)
-
-  assert(firstElContents.includes('I like Jason because he travels to places.'))
-})
-
-// check POST request - valid blog created - initialBlogs increase by one
-test('a valid blog POST', async () => {
-  const newBlog = 
-    {
-      "user": "John Doe",
-      "comment": "some comment here",
-      "likes": 1,
+  // likes vlaue default
+  test('likes value - default to zero', async () => {
+    const newBlog = {
+      "user": "Name",
+      "comment": "comment",
+      "likes": "",
       "date": "today"
     }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+    await api 
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
 
-  const response = await blogsInDb()
+    const response = await blogsInDb()
+    //const test = response[response.length - 1]['likes']
+    const lastBlogLikes = response[response.length - 1]['likes']
 
-  const contents = response.map(el => el.comment)
+    assert.strictEqual(lastBlogLikes, 0)
+  })
 
-  assert.strictEqual(response.length, initialBlogs.length + 1)
-  assert(contents.includes('some comment here'))
+  // must have user and comment content
+  test('must have user and comment inputs', async () => {
+    const newBlog = {
+      "user": "",
+      "comment": "",
+      "likes": "",
+      "date": ""
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  })
+
+  // check POST request - valid blog created - initialBlogs increase by one
+  test('a valid blog POST', async () => {
+    const newBlog = 
+      {
+        "user": "John Doe",
+        "comment": "some comment here",
+        "likes": 1,
+        "date": "today"
+      }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await blogsInDb()
+
+    const contents = response.map(el => el.comment)
+
+    assert.strictEqual(response.length, initialBlogs.length + 1)
+    assert(contents.includes('some comment here'))
+  })
 })
 
-test('blog malformed - invalid not added', async () => {
-  const newBlog = {
-    "comment": "",
-    "likes": "",
-    "date": ""
-  }
+//////////////////////////////////////////////////////////////////////
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+describe('tests for one specific blogs resource', () => {
+  // test for specific blog in DB
+  
+  // checks all blogs content from mongoDB storage
+  test('blogs content check', async () => {
+    const response = await api.get('/api/blogs')
 
-  const blogsResult = await blogsInDb()
+    const contents = response.body.map(el => el.comment)
+    assert(contents.includes('I like Jason because he travels to places.'))
+  })
 
-  assert.strictEqual(blogsResult.length, initialBlogs.length)  
-})
+  // checks one blogs content of first array item for mongoDB storage
+  test('blogs content -- first item -- check', async () => {
+    const response = await api.get('/api/blogs')
+    const firstElContents = response.body.map(el => el.comment)
 
-test('likes value - default to zero', async () => {
-  const newBlog = {
-    "user": "Name",
-    "comment": "comment",
-    "likes": "",
-    "date": "today"
-  }
-
-  await api 
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(200)
-
-  const response = await blogsInDb()
-  //const test = response[response.length - 1]['likes']
-  const lastBlogLikes = response[response.length - 1]['likes']
-
-  assert.strictEqual(lastBlogLikes, 0)
-})
-
-test('must have user and comment inputs', async () => {
-  const newBlog = {
-    "user": "",
-    "comment": "",
-    "likes": "",
-    "date": ""
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+    assert(firstElContents.includes('I like Jason because he travels to places.'))
+  })
 })
 
 after(async () => {
